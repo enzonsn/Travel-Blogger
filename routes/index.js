dcca8069bdeafbfa70a0d07214ec1e5b060566aa
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-20 19:06:03 -0500

    added routes to server file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./User-routes.js');
+const userRoutes = require('./User-routes');
 const postRoutes = require('./Post-routes');
 // const commentRoutes = require('./comment-routes');
 
