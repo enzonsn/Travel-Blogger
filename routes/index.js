0363b4b5a554aba01908ecc432b2c77b78271245
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-22 17:47:23 -0500

    added home-routes and installed express-handlebars

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./User-routes');
 const postRoutes = require('./Post-routes');
+const homeRoutes = require('./homepage-routes');
 // const commentRoutes = require('./comment-routes');
 
 // router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/home', homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
+const exphbs = require('express-handlebars');
+const helpers = require('./');
+const hbs = exphbs.create({ helpers });
 
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
