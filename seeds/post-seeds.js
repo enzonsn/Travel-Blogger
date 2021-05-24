247160124e359b9bf9030a0d35080f77984fd261
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-24 12:26:24 -0500

    added user id to post model and removed if statement from home route. added more seeds.

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,12 +4,17 @@ const postData = [
     {
         post_content: "Travel Blog is up and running!",
         post_url: "Testing",
-        /* user_id: 1 */
+         user_id: 1
     },
     {
         post_content: "Travel Blog is up and running 2!",
         post_url: "Test",
-        /* user_id: 2 */
+        user_id: 2
+    },
+    {
+        post_content: "Travel Blog is up and running 3!",
+        post_url: "Tester",
+        user_id: 3
     },
 ]
 
