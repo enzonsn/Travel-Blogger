1cee98dab2b630b1a95493a56518c740b068b952
Chris Ruiz <chrispruiz@att.net>
2021-05-26 18:23:37 -0500

    added about page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const postRoutes = require('./Post-routes');
 const homeRoutes = require('./homepage-routes');
 const landingRoutes = require('./landing-routes');
 const profileRoutes = require('./profile-page-routes');
+const aboutRoutes = require('./about-routes');
 // const commentRoutes = require('./comment-routes');
 
 // router.use('/comments', commentRoutes);
@@ -14,5 +15,6 @@ router.use('/posts', postRoutes);
 router.use('/home', homeRoutes);
 router.use('/', landingRoutes);
 router.use('/profile', profileRoutes);
+router.use('/about', aboutRoutes);
 
 module.exports = router;
