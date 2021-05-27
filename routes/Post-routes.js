5e926ae264c95b9301e175a16f2ccf4300542438
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-26 21:24:16 -0500

    working on deleting posts

--- routes/Post-routes.js
+++ routes/Post-routes.js
@@ -94,6 +94,7 @@ router.put("/:id", (req, res) => {
 
 
 
+
 // delete a post
 router.delete("/:id", (req, res) => {
     Post.destroy({
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash())
 app.use(session({
