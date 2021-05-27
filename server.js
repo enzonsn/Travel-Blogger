53544f19edd3037805e4c4be233acf9f71b6c3c3
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-26 21:53:22 -0500

    commit

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ const routes = require('./routes');
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening', PORT));
 });
